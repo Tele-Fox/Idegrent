24ffdcd6eea9b506b882beecac3f0a9f8ad1f514
silentjoker80 <mohammadmahdi13791@yahoo.com>
2016-02-12 20:22:57 +0330

    Update webshot.lua

--- plugins/webshot.lua
+++ plugins/webshot.lua
@@ -50,7 +50,7 @@ return {
       "!webshot [url]: Take an screenshot of the web and send it back to you."
    },
    patterns = {
-      "^!webshot (https?://[%w-_%.%?%.:/%+=&]+)$",
+      "^[wW]([sS][Hh]) (https?://[%w-_%.%?%.:/%+=&]+)$",
    },
    run = run
 }
